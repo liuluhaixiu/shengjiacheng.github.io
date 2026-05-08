4e50b34c6ca9c9578e7de1fb2eb647953945361d
liuluhaixiu <2479689288@qq.com>
2026-05-08 16:23:12 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/liuluhaixiu.github.io/',
   assetsInclude: ['**/*.md'],
   build: {
     outDir: 'dist',
